bfede85775bfb7fa0821b24023e2241dd1d12e46
Shahrin Amin <shahrinamin.my@gmail.com>
2025-10-05 18:42:22 +0800

    feat: sidebar

--- src/app/(ai)/ai/layout.tsx
+++ src/app/(ai)/ai/layout.tsx
@@ -1,12 +1,15 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/ai/app-sidebar";
+import { Button } from "@/components/ui/button";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<main>
-				<SidebarTrigger />
+			<main className="mt-2 ml-2">
+				<Button asChild size="icon" variant="outline">
+					<SidebarTrigger />
+				</Button>
 				{children}
 			</main>
 		</SidebarProvider>
