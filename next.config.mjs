e245b7575cf9f3256f1cad8453ae5aea61bf186e
Shahrin Amin <shahrinamin.my@gmail.com>
2025-10-06 10:22:02 +0800

    fix: .css error

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add MiniCssExtractPlugin to the plugins array
-    config.plugins.push(new MiniCssExtractPlugin());
+	output: "standalone",
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+		// Add MiniCssExtractPlugin to the plugins array
+		// config.plugins.push(new MiniCssExtractPlugin());
 
-    // Important: return the modified config
-    return config;
-  },
+		// Important: return the modified config
+		return config;
+	},
 };
 
 export default nextConfig;
