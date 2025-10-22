7f4bd165752fc5e9c160ae38afdc1fd626e10ba4
Khairul Imran <khaiimran72@gmail.com>
2025-10-22 15:19:19 +0800

    feature: tableau init

---
+++ src/components/dashboard/tableau/TableauEmbedv2.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import Script from "next/script";
+
+interface TableauEmbedProps {
+	src: string;
+	height?: string;
+	width?: string;
+	hideTabs?: boolean;
+	hideToolbar?: boolean;
+	device?: "default" | "desktop" | "tablet" | "phone";
+}
+
+function TableauEmbed({
+	src,
+	height = "600px",
+	width = "100%",
+	hideTabs = false,
+	hideToolbar = false,
+	device = "default",
+}: TableauEmbedProps) {
+	const vizRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (vizRef.current) {
+			const viz = vizRef.current.querySelector("tableau-viz");
+			if (viz) {
+				viz.setAttribute("src", src);
+				viz.setAttribute("height", height);
+				viz.setAttribute("width", width);
+				if (hideTabs) viz.setAttribute("hide-tabs", "");
+				if (hideToolbar) viz.setAttribute("toolbar", "hidden");
+				viz.setAttribute("device", device);
+			}
+		}
+	}, [src, height, width, hideTabs, hideToolbar, device]);
+
+	return (
+		<>
+			<Script
+				type="module"
+				src="https://100.83.250.224:8080/javascripts/api/tableau.embedding.3.latest.min.js"
+				strategy="lazyOnload"
+			/>
+			<div ref={vizRef}>
+				{/* @ts-ignore */}
+				<tableau-viz id="tableauViz"></tableau-viz>
+			</div>
+		</>
+	);
+}
+export default TableauEmbed;
---
+++ src/components/dashboard/tableau/TableauServerEmbed.tsx
@@ -0,0 +1,56 @@
+import Script from "next/script";
+
+interface TableauEmbedServerProps {
+	viewUrl: string;
+	height?: string;
+	width?: string;
+	hideTabs?: boolean;
+	hideToolbar?: boolean;
+	device?: "default" | "desktop" | "tablet" | "phone";
+}
+
+async function TableauEmbedServer({
+	viewUrl,
+	height = "600px",
+	width = "100%",
+	hideTabs = false,
+	hideToolbar = false,
+	device = "default",
+}: TableauEmbedServerProps) {
+	// Fetch the Tableau ticket from the API route
+	const response = await fetch("/api/get-tableau-ticket", {
+		method: "POST",
+		cache: "no-store",
+	});
+	const { ticket } = await response.json();
+
+	const trustedUrl = `http://100.83.250.224:8080/trusted/${ticket}/${viewUrl}?:embed=yes&:toolbar=${
+		hideToolbar ? "no" : "yes"
+	}&:tabs=${hideTabs ? "no" : "yes"}&:device=${device}`;
+
+	return (
+		<>
+			<Script
+				type="module"
+				src="https://100.83.250.224:8080/javascripts/api/tableau.embedding.3.latest.min.js"
+				strategy="lazyOnload"
+			/>
+			<div>
+				{/* @ts-ignore */}
+				<tableau-viz
+					id="tableauViz"
+					src={trustedUrl}
+					height={height}
+					width={width}
+					device={device}
+					toolbar={hideToolbar ? "hidden" : "bottom"}
+					hide-tabs={hideTabs ? "" : undefined}
+				>
+					{/* @ts-ignore */}
+				</tableau-viz>
+			</div>
+		</>
+	);
+}
+
+export default TableauEmbedServer;
